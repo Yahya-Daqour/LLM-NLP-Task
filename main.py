b23b0f65c2af4ae7f97419cc173f53531b063abd
Yahya-Daqour <yahyadaqour@gmail.com>
2025-08-02 23:33:36 +0300

    split script into functions

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import kagglehub
 import pandas as pd
 from utils import TextPreprocessor
 import os
-from model import SVM
+from model import SVMModel, LSTMModel
 import logging
-
+import tensorflow as tf
 logging.basicConfig(
      format="{asctime} - {levelname} - {message}",
      style="{",
@@ -12,37 +12,75 @@ logging.basicConfig(
      level=logging.DEBUG
  )
 
-# Download kaggle dataset
-logging.info("Downloading dataset")
-path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
-print("Path to dataset files:", path)
 
-# Read data
-df = pd.read_csv(path + "/IMDB Dataset.csv")
-print(df)
+def import_data():
+    # Download kaggle dataset
+    logging.info("Downloading dataset")
+    path = kagglehub.dataset_download("lakshmi25npathi/imdb-dataset-of-50k-movie-reviews")
+    print("Path to dataset files:", path)
 
-# Preprocess
-logging.info("Preprocess Data...")
-if os.path.exists("outputs/output_dataframe.pkl"):
-    logging.info("Data already exists")
-    df_processed = pd.read_pickle("outputs/output_dataframe.pkl")
+    # Read data
+    df = pd.read_csv(path + "/IMDB Dataset.csv")
+    return df
+
+def preprocess_data(df):
+    logging.info("Preprocess Data...")
+    if os.path.exists("outputs/output_dataframe.pkl"):
+        logging.info("Data already exists")
+        df_processed = pd.read_pickle("outputs/output_dataframe.pkl")
+        
+
+    else:
+        logging.info("Preprocessing Data...")
+        preprocessor = TextPreprocessor()
+        df_processed = preprocessor.preprocess_text(df)
+        preprocessor.save_pickle(df_processed)
+        logging.info("Preprocessing Done...")
+
+    df['label'] = df['sentiment'].map({'positive': 1, 'negative': 0})
+    df_processed['label'] = df_processed['sentiment'].map({'positive': 1, 'negative': 0})
+    return df, df_processed
+
+
+def call_model(model_type, df):
+    logging.info("Calling model for training...")
+    if model_type.lower() == "svm":
+        model = SVMModel()
+    elif model_type.lower() == "lstm":
+        model = LSTMModel()
     
+    X, y = model.tokenizer(df["review"], df["label"])
+    X_train, X_test, y_train, y_test = model.split_data(X, y)
+    
+    if type(model) is SVMModel:
+        model.fit(X_train, y_train)
+    elif type(model) is LSTMModel:
+        logging.info(f"Num GPUs Available: {len(tf.config.list_physical_devices('GPU'))}")
+        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3)
+    
+    logging.info("Done training")
+    return model, X_test, y_test
 
-else:
-    logging.info("Preprocessing Data...")
-    preprocessor = TextPreprocessor()
-    df_processed = preprocessor.preprocess_text(df)
-    preprocessor.save_pickle(df_processed)
-    logging.info("Preprocessing Done...")
+def eval_model(X_test, y_test):
+    logging.info("Evaluating the model...")
+    model.eval(X_test, y_test)
 
-# Call model
-logging.info("Calling model for training...")
-svm_model = SVM()
-X,y = svm_model.tokenizer(df_processed["review"], df_processed["sentiment"])
-X_train, X_test, y_train, y_test = svm_model.split_data(X, y)
-svm_model.fit(X_train, y_train)
-logging.info("Done training")
+if __name__== "__main__":
+    df = import_data()
 
-# Evaluate
-logging.info("Evaluating the model...")
-svm_model.eval(X_test, y_test)
+    # 1.Text Processing
+    df, df_processed = preprocess_data(df)
+
+    # 2. Machine Learning model for sentiment classification
+    model, X_test, y_test = call_model("svm", df_processed)
+    eval_model(X_test, y_test)
+
+    # 3.1. LSTM Model with preprocessed data
+    model, X_test, y_test = call_model("lstm", df_processed)
+    eval_model(X_test, y_test)
+
+    # 3.2. LSTM Model with original data
+    model, X_test, y_test = call_model("lstm", df)
+    eval_model(X_test, y_test)
+
+    # 4. GPT-2 Classification using Prompt Engineering
